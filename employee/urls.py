0fe442690373e029cb199bf877f5b9a327f0847f
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2021-09-14 16:36:23 -0300

    fixed url from 'employee/employee/' to 'employee/'

--- employee/urls.py
+++ employee/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'employees', views.EmployeeViewSet)
+router.register(r'employee', views.EmployeeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
