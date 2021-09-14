de9150f8b8d8e26bdd843a3c44ce535d46d721ff
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2021-09-14 16:20:41 -0300

    added Employee serializer to views.py

--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+
+from .models import Employee
+from .serializers import EmployeeSerializer
 
 def index(request):
     return HttpResponse("hello from employee/")
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
