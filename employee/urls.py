c81fa08e214238e4a6d3c2e028bed47766f927ea
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2021-09-14 16:35:33 -0300

    added EmployeeViewSet to urls.py

--- employee/urls.py
+++ employee/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'employees', views.EmployeeViewSet)
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
+    path('api/', include('rest_framework.urls', namespace='rest_framework')),
 ]
